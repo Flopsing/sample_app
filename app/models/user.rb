7c79394c8f62b4be1091c133b12311d1309bbfbf
Fabian Seidel <f.seidel@x-ion.de>
2013-07-17 13:34:13 +0200

    added remember token to user model. added test for remember token to user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,9 @@
 class User < ActiveRecord::Base 
   attr_accessible :name, :email,:password, :password_confirmation
   before_save {|user| user.email = email.downcase}
+  before_save :create_remember_token
   has_secure_password
+
   
   validates :name, presence: true, length: {maximum:50}
   validates :password, presence: true, length: { minimum: 6 }
@@ -24,6 +26,12 @@ class User < ActiveRecord::Base
   #same as
   #validates(:name, presence: true)
   #but because the hash is the last argument the curly braces are not needed
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 
 end
 
