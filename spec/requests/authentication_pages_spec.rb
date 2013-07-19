8e6ddfea5964473f6903ca6563ebd21ce6866adc
Fabian Seidel <f.seidel@x-ion.de>
2013-07-19 11:23:21 +0200

    added admin attribute to user model

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -31,8 +31,8 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      #before do
-      #  fill_in "Email",    with: user.email.upcase
+      #before fill_incom
+      #  do "Email",    with: user.email.upcase
       #  fill_in "Password", with: user.password
       #  click_button "Sign in"
       #end
